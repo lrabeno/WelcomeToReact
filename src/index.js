1eda64b67faf6d5ec91696b8d173078cf1fb269e
Louis Rabeno <lrabeno@gmail.com>
2022-10-12 22:16:12 -0400

    added react code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Your code here!
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Counter = () => {
+  return (
+    <div id="container">
+      <div id="navbar">Counter.js</div>
+      <div id="counter">
+        <h1>0</h1>
+        <button>Increment</button>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<Counter />, document.getElementById("app"));
